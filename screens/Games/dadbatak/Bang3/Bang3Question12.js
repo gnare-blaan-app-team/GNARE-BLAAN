e1d3fa67c0702b15ca71d0a502f547cceb0c1a8f
jonardtanilon <jonardTanilon98@gmail.com>
2019-06-25 16:05:17 +0800

    All bang 3

---
+++ screens/Games/dadbatak/Bang3/Bang3Question12.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { Text, View, Image, StyleSheet, TouchableOpacity , ImageBackground } from 'react-native';
+import { withNavigation } from 'react-navigation';
+
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import {globalStyleSheet} from '../../../globalStyleSheet/globalStyleSheet';
+import GnareIcon from '../../gameImages/GnareMain.png';
+import GameBG from '../../gameImages/GameBG.png';
+import Home_icon from '../../../images/Home_icon.png';
+import Bang3Q12 from '../../gameImages/bang3Images/db_bang3Q12.png';
+import Waterman from '../../gameImages/bang3Images/waterman.png';
+import Tubeman from '../../gameImages/bang3Images/tubeman.png';
+import Fireman from '../../gameImages/bang3Images/fireman.png';
+
+class Bang3Question12 extends Component {
+    static navigationOptions = {
+        header: null,
+    }
+
+    gotoMainMenu = () =>{
+        this.props.navigation.navigate('mainMenu')
+    }
+
+    render() {
+        return (
+            <ImageBackground
+                source={GameBG}
+                style={globalStyleSheet.image}
+            >
+                <View style={globalStyleSheet.gnareIconStyle}>
+                        <Image
+                            source={GnareIcon}
+                            style={styles.image}
+                        >
+                        </Image>
+                </View>
+                <View style={globalStyleSheet.homeContainer}>
+                    <TouchableOpacity onPress={this.gotoMainMenu}>
+                        <Image
+                            source={Home_icon}
+                            style={globalStyleSheet.home}
+                        ></Image>
+                    </TouchableOpacity>
+                </View>
+                <View style={globalStyleSheet.questionStyle}>
+                    <Image
+                        source={Bang3Q12}
+                        style={styles.image}
+                    >
+                    </Image>
+                </View>
+                <View style={globalStyleSheet.db_b3_choice1Style}>
+                    <TouchableOpacity>
+                        <Image
+                            source={Waterman}
+                            style={styles.image}
+                        >
+                        </Image>
+                    </TouchableOpacity>
+                </View>
+                <View style={globalStyleSheet.db_b3_choice2Style}>
+                    <TouchableOpacity>
+                        <Image
+                            source={Tubeman}
+                            style={styles.image}
+                        >
+                        </Image>
+                    </TouchableOpacity>
+                </View>
+                <View style={globalStyleSheet.db_b3_choice3Style}>
+                    <TouchableOpacity>
+                        <Image
+                            source={Fireman}
+                            style={styles.image}
+                        >
+                        </Image>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'stretch'
+    },
+    
+})
+
+export default withNavigation(Bang3Question12);
